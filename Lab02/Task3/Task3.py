39b3e05dbb35fe566b8fcf3b51740155d02c6d7f
mattpopovich <popovich.matt@gmail.com>
2016-01-26 10:35:58 -0500

    Finished Task 3

--- Lab02/Task3/Task3.py
+++ Lab02/Task3/Task3.py
@@ -30,7 +30,31 @@ def encryptFile(key, iv, mode, in_filename, out_filename=None, chunksize=1024):
                     chunk += ' ' * (16 - len(chunk) % 16) #pad the last block with spaces
                 outfile.write(encryptor.encrypt(chunk))
 
-
+# Decrypt a file using ECB and CBC
+def decryptFile(key, iv, mode, in_filename, out_filename=None, chunksize=1024):
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+    if mode == AES.MODE_ECB:
+        # no iv is needed for ECB!
+        encryptor = AES.new(key, AES.MODE_ECB)
+    elif mode == AES.MODE_CBC:
+        encryptor = AES.new(key, AES.MODE_CBC, iv)
+    else:
+        print "invalid mode, program stopped"
+        return
+    with open(in_filename, 'rb') as infile:
+        # extract bmp header
+        #header = infile.read(54)
+        # open output file
+        with open(out_filename, 'wb') as outfile:
+            #outfile.write(header)
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16) #pad the last block with spaces
+                outfile.write(encryptor.decrypt(chunk))
 
 
 # Create 64 byte long text file
@@ -40,11 +64,11 @@ f.write('This file is 64 bytes in length and is to be encrypted via AES!!')
 f.close()
 print u'\u2713'
 
+
 # Encrypt the file using the AES-128 cipher
 print "Encrypting the file (64bytes.txt) with AES-ECB......",
 encryptFile('mysecretpassword', 'N/A', AES.MODE_ECB, '64bytes.txt', '64bytesECB.txt')
 print u'\u2713'
-
 
 print "Encrypting the file (64bytes.txt) with AES-CBC......",
 encryptFile('mysecretpassword', '1234567890123456', AES.MODE_CBC, '64bytes.txt', '64bytesCBC.txt')
@@ -79,9 +103,18 @@ f.close()
 print u'\u2713'
 
 
-
 # Decrypt the corrupted, encrypted file using the correct key and IV
+print "Decrypting 64bytesECBcorrupt.txt with AES-ECB.......",
+decryptFile('mysecretpassword', 'N/A', AES.MODE_ECB, '64bytesECBcorrupt.txt', '64bytesECBcorruptDecrypt.txt')
+decryptFile('mysecretpassword', 'N/A', AES.MODE_ECB, '64bytesECB.txt', '64bytesECBDecrypt.txt')
+print u'\u2713'
 
+print "Decrypting 64bytesCBCcorrupt.txt with AES-CBC.......",
+decryptFile('mysecretpassword', '1234567890123456', AES.MODE_CBC, '64bytesCBCcorrupt.txt', '64bytesCBCcorruptDecrypt.txt')
+decryptFile('mysecretpassword', '1234567890123456', AES.MODE_CBC, '64bytesCBC.txt', '64bytesCBCDecrypt.txt')
+print u'\u2713'
+
+print "Finished"
 
 
 
