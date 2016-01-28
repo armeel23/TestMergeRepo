02904d318e1f2aae8019eafb1f61a8cca3d1be83
mattpopovich <popovich.matt@gmail.com>
2016-01-27 23:16:52 -0500

    Finished Task 4

--- Lab02/Task4/Task4.py
+++ Lab02/Task4/Task4.py
@@ -2,77 +2,71 @@
 
 # IMPORTS
 from Crypto.Cipher import AES
-import binascii
-import struct
 
 # FUNCTION DEFINITIONS
 # Encrypt a file using ECB and CBC
-def encryptPlaintext(key, iv, mode, plaintext, chunksize=1024):
+def encryptPlaintext(key, iv, mode, plaintext, chunksize=16):
 
-    if mode == AES.MODE_ECB:
-        # no iv is needed for ECB!
-        encryptor = AES.new(key, AES.MODE_ECB)
-    elif mode == AES.MODE_CBC:
+    if mode == AES.MODE_CBC:
         encryptor = AES.new(key, AES.MODE_CBC, iv)
     else:
         print "invalid mode, program stopped"
         return
 
-    f = open('temp.txt', 'wb')
+    output = ''
     while True:
         chunk = plaintext[0:chunksize]
         plaintext = plaintext[chunksize:]
         if len(chunk) == 0:
             break
         elif len(chunk) % 16 != 0:
-            chunk += ' ' * (16 - len(chunk) % 16) #pad the last block with spaces
+            chunk += (16 - len(chunk) % 16) * chr(16 - len(chunk) % 16) # Pad the last block with PKCS5 padding: https://gist.github.com/crmccreary/5610068
+        output += str(encryptor.encrypt(chunk).encode('hex'))   # Append the current block
 
-        f.write(encryptor.encrypt(chunk))
-    f.close()
-    f = open('temp.txt', 'rb')
-    output = f.readline()
     return output
 
 
-ciphertext = 0x3f814d00c3f1047f1dfa879115970472472a17eabdd9ba4fcd667743e1e03674
+ciphertext = '3f814d00c3f1047f1dfa879115970472472a17eabdd9ba4fcd667743e1e03674'
 plaintext = 'This is a top secret.'
 iv = '\0' * 16      # 16 NULL characters
 
-f = open('words2.txt', 'r')
-max = 0
+f = open('words.txt', 'r')
+print "Searching for the key",
+count = 0
+found = False
 for line in f.readlines():
-    # Make the line 16 characters
+    line = line.strip()                 # Remove spaces and newlines from word read
+    # Make the line 16 characters, some were up to 24 characters and no specification was given for what to do with them
     line = line[0:16]
-    if (len(line) <= 18):
-        spaces = '                '
-        key = line[0:-2]           # Lines end in 0x0d 0x0a ("\r\n")
-        key = key[0:len(key)] + spaces[0:(16-len(key))]
-        # Is key the correct key?
-        # Encrypt the plaintext with the line
-        encryptedPlaintext = encryptPlaintext(key, iv, AES.MODE_CBC, plaintext)
-        hexEncryptedPlaintext = ":".join("{:02x}".format(ord(c)) for c in encryptedPlaintext)
-        hexEncryptedPlaintext = hexEncryptedPlaintext.replace(":", "")
-        """print hexEncryptedPlaintext
-        print type(hexEncryptedPlaintext)
-        print len(hexEncryptedPlaintext)
-        print hex(ciphertext)[2:-1]
-        print type(hex(ciphertext)[2:-1])
-        print len(hex(ciphertext)[2:-1])"""
+    spaces = '                '
+    key = line + spaces[0:(16-len(line))]  # Pad the word read from the file with spaces
         
-        if ( hexEncryptedPlaintext[0:5] == hex(ciphertext)[2:-1][0:5] ):
-            print "key: ",
-            print key
-            print "Key ciphertext: ",
-            print hexEncryptedPlaintext
-            print "ciphertext:     ",
-            print hex(ciphertext)[2:-1]
+    # Is key the correct key?
+    # Encrypt the plaintext with the key
+    encryptedPlaintext = encryptPlaintext(key, iv, AES.MODE_CBC, plaintext)
     
-    
-            line = ''
-    else :
-        print len(line)
-        print line
-        print "Error, dictionary word is too long"
+    if (encryptedPlaintext == ciphertext):
+        found = True
+        print u'\u2713'
+        print "Key:                ",
+        print key
+        print "Key Ciphertext:     ",
+        print encryptedPlaintext
+        print "Original Ciphertext:",
+        print ciphertext
 
+    line = ''   # Clear the line and continue the search
+
+    count += 1
+    if ((count % 1000 == 0) and not(found)):
+        print '.',  # Simulate thinking by printing a dot after every 1k tested keys
 
 f.close()
+
+
+""" OUTPUT:
+    Searching for the key . . . . . . . . . . y
+    Key:                 hack
+    Key Ciphertext:      3f814d00c3f1047f1dfa879115970472472a17eabdd9ba4fcd667743e1e03674
+    Original Ciphertext: 3f814d00c3f1047f1dfa879115970472472a17eabdd9ba4fcd667743e1e03674
+"""
