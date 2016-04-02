696d2a67a66475054c7581baaeb1f2b0b16bb59b
mattpopovich <popovich.matt@gmail.com>
2016-04-01 20:49:56 -0400

    Reading file byte by byte

--- CTF/circuit.py
+++ CTF/circuit.py
@@ -1,3 +1,25 @@
 # Python script to simulate a circuit for CTF
 
+infile = 'exampleFile.txt'
+fin = open(infile, 'r')
+outfile = 'outFile.txt'
+fout = open(outfile, 'w')
+
+intext = fin.read()
+
+
+# Loop through infile byte by byte
+while (len(intext) >= 8):
+    
+    
+    
+    
+    
+    print intext[0:8]
+    fout.write(intext[0:8])
+    intext = intext[8:]
+
+
+
+
 
